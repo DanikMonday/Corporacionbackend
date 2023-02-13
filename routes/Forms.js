28721f1399293233f326b1c9c08273fafc8447ee
Óscar O <oscarforjuela@hotmail.com>
2023-02-13 12:08:06 -0500

    New route Form

--- routes/Forms.js
+++ routes/Forms.js
@@ -1,5 +1,17 @@
 const router = require("express").Router();
 const FormModel = require("../models/Form");
 
+router.post("/new", async (req, res)=>{
+    try {
+        const {type, nit_cedula, name, email, phone, destination_don, certification, aditiona, state} = req.body;
+        const newForm = await FormModel.create({
+            type, nit_cedula, name, email, phone, destination_don, certification, aditiona, state
+        });
+        res.send({data: newForm});
+    } catch (error) {
+        console.log(error);        
+    }
+});
+
 
 module.exports = router;
