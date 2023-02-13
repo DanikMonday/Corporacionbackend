c094f9b051f370818f7205665d8356f3ab8ddcde
Óscar O <oscarforjuela@hotmail.com>
2023-02-13 12:16:58 -0500

    Route Form get

--- routes/Forms.js
+++ routes/Forms.js
@@ -1,6 +1,17 @@
 const router = require("express").Router();
 const FormModel = require("../models/Form");
 
+//Consultar formularios
+router.get("/table", async (req, res)=>{
+    try {
+        const tableForms = await FormModel.find();
+        res.json(tableForms)
+    } catch (error) {
+        console.log(error);
+    }
+});
+
+//Crear Formulario
 router.post("/new", async (req, res)=>{
     try {
         const {type, nit_cedula, name, email, phone, destination_don, certification, aditiona, state} = req.body;
