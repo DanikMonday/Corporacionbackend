02a8d6bac3bf486a13c2df977ad1e35c580acf21
Óscar O <oscarforjuela@hotmail.com>
2023-03-02 10:48:46 -0500

    New Recovery Password

--- routes/user.js
+++ routes/user.js
@@ -55,9 +55,10 @@ router.post("/recovery", async (req, res)=>{
     const {email} = req.body;
     const user = await UserModel.findOne({email});
     if(user){
-        let a = "asd123";
+        let a = "asd12345";
         const newPass = await UserModel.findByIdAndUpdate(user.id, {password: a});
-        recoveryMail(user);
+        const nUser = await UserModel.findOne({email});
+        recoveryMail(nUser);
         res.send("Correo enviado");
     }else{
         res.send({error: "No se encuentra registrado el correo"});
